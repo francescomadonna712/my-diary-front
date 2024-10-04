d46134ece0f92e7daf94792660401c4b3b596ff7
francesco madonna <160396542+francescomadonna712@users.noreply.github.com>
2024-10-04 09:40:14 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+import AppFotter from "./components/AppFotter.vue";
+
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+    AppFotter,
+  },
+
+  data() {
+    return {
+
+    }
+  },
+}
+
+</script>
+
+<template>
+
+  <AppHeader />
+  <AppMain />
+  <AppFooter />
+
+</template>
+
+<style scoped></style>
